52eb1f223babe0cb8b005e52c5325fe0fe27ca03
bb <biletskyboh@gmail.com>
2017-04-22 14:55:35 +0300

    added form with custom validation

--- forms.py
+++ forms.py
@@ -2,6 +2,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired, Email
 
+from models import Client
+
+
 class RegisterForm(FlaskForm):
     first_name = StringField(
                         'First Name',
@@ -16,17 +19,36 @@ class RegisterForm(FlaskForm):
                                     )]
                         )
     email = StringField(
-                        'Last Name',
-                        [DataRequired(message='The email is already taken.')],
-                        [Email(
+                        'Email',
+                        [
+                        DataRequired(message='You have to enter an email.'),
+                        Email(
                             message='You have to enter a valid email address'
-                            )]
+                            )],
                         )
     passport_number = StringField(
-                                'Last Name',
+                                'Passport',
                                 [DataRequired(
                                     message='You have to enter a passport_number'
                                     )],
-
                                 )
+
+    def validate(self):
+        rv = FlaskForm.validate(self)
+        if not rv:
+            return False
+
+        client = Client.query.filter_by(email=self.email.data).first()
+        if not client is None:
+            self.email.errors.append('The email is already taken.')
+            return False
+
+        client = Client.query.filter_by(
+                            passport_number=self.passport_number.data).first()
+        if not client is None:
+            self.passport_number.errors.append(
+                                    'The passport number is already taken.')
+            return False
+
+        return True
 
