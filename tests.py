f0c82b1f5d7617fa4f5efefb3a6d848e582895d4
bb <biletskyboh@gmail.com>
2017-04-22 08:21:57 +0300

    fixed test

--- tests.py
+++ tests.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 import models
+from finsystem import app
 
 
 class RolesModelTestCase(unittest.TestCase):
@@ -51,12 +52,10 @@ class RegisterEndpointTestCase(unittest.TestCase):
                                                 bind=self.engine))
         Base.query = self.session.query_property()
         Base.metadata.create_all(bind=self.engine)
-        self.client_info = {
-                    id=1, first_name='John', last_name='Doe', 
-                    email='johndoe@mail.com'}
+        self.app = app.test_client()
 
     def register(
-                self, first_name, last_name, email=None,
+                self, first_name, last_name, email,
                 password, password2=None,):
         """Use helper function to register a user."""
         
@@ -64,7 +63,7 @@ class RegisterEndpointTestCase(unittest.TestCase):
             password2 = password
         if email is None:
             email = username + '@example.com'
-        return self.post('/register', data={
+        return self.app.post('/register', data={
                                             'first_name': first_name,
                                             'last_name': first_name,
                                             'email': email,
@@ -74,20 +73,20 @@ class RegisterEndpointTestCase(unittest.TestCase):
 
         
     def test_register_endpoint(self):
-       """Make sure registering works"""
-        rv = register(self, 'user1', 'default')
+        """Make sure registering works."""
+        rv = self.register('user1', 'default', 'meh@mail', '1234hello')
         assert b'Your account waits for approval.' in rv.data
-        rv = register(, '', 'default')
+        rv = self.register('', 'default', 'meh@mail', '1234hello')
         assert b'You have to enter a first_name' in rv.data
-        rv = register(self, 'meh', '')
+        rv = self.register('meh', '', 'meh@mail', '1234hello')
         assert b'You have to enter a last_name' in rv.data
-        rv = register(self, 'user1', 'default', 'admin@mail.com')
+        rv = self.register('user1', 'default', 'admin@mail.com', '1234hello')
         assert b'The email is already taken.' in rv.data
-        rv = register(self, 'meh', 'foo', email='broken')
-        assert b'You have to enter a valid email address' in rv.data 
-        rv = register(self, 'meh', 'beh', 'meh@mail.com', '',)
+        rv = self.register('meh', 'foo', 'broken', '1234hello', '1234hello')
+        assert b'You have to enter a valid email address' in rv.data
+        rv = self.register('meh', 'beh', 'meh@mail.com', '')
         assert b'You have to enter a password' in rv.data
-        rv = register(self, 'meh', 'beh', 'meh@mail', '1234hello', '1234hey')
+        rv = self.register('meh', 'beh', 'meh@mail', '1234hello', '1234hey')
         assert b'The two passwords do not match' in rv.data
         
     def tearDown(self):
