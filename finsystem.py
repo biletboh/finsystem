62d9d8ae2167e90e6fa8dbfb8d753bdfbc313728
bb <biletskyboh@gmail.com>
2017-04-22 14:58:42 +0300

    updated register view and added minimal register templates

--- finsystem.py
+++ finsystem.py
@@ -1,19 +1,54 @@
-from flask import Flask
+from flask import Flask, request, redirect, render_template, flash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import Base
+from flask_security import Security, SQLAlchemyUserDatastore, \
+    UserMixin, RoleMixin, login_required
+from models import Base, Client, ApprovalList, Role
+from forms import RegisterForm
 
 
+# Create application
 app = Flask(__name__)
 
+
+app.config.update(dict(
+    DEBUG=True,
+    SECRET_KEY='development key',
+    WTF_CSRF_SECRET_KEY='secret csrf key',
+    ))
 
 engine = create_engine('sqlite:///roles.db')
 Base.metadata.bind = engine
 
+# Setup Flask-Security
+user_datastore = SQLAlchemyUserDatastore(Base, Client, Role)
+security = Security(app, user_datastore, register_form=RegisterForm)
 
-@app.route('/')
-def Index():
-    return "Hello Finsystem"
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/register', methods=['GET', 'POST'])
+def RegisterClient():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        email = form.email.data
+        passport_number = form.passport_number.data
+        to_approve = ApprovalList(
+                                first_name=first_name,
+                                last_name=last_name, 
+                                email=email,
+                                passport_number=passport_number)
+        flash('Your account waits for approval.')
+        return "validation works" 
+
+    return render_template('customregister.html', form=form)
+
+
+@app.route('/success', methods=['GET'])
+def WaitForApproval():
+    return 'Your account waits for approval.'
 
 if __name__ == '__main__':
     app.debug = True
