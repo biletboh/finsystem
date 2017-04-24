c1aba60e32c56399d5e552298f6d8185be1588ad
bb <biletskyboh@gmail.com>
2017-04-24 12:58:57 +0300

    add superuser function

---
+++ add_superuser.py
@@ -0,0 +1,35 @@
+from flask_security.utils import encrypt_password
+from finapp import app, db
+from security import user_datastore
+from models import Role, Manager
+
+
+#  Run this script to create Superuser
+def create_roles_and_superuser():
+    with app.app_context():
+
+        sup_role = Role(
+                name='superuser', 
+                description='This user can create managers.'
+                )
+
+        man_role = Role(
+            name='manager', 
+            description='Managers can approve client accounts')
+        db.session.add(sup_role)
+        db.session.add(man_role)
+        db.session.commit()
+
+        #  Customize your super user on production
+        superuser = user_datastore.create_user(
+                username='SuperManager', email='admin@mail.com',
+                password=encrypt_password('superuserpass'),
+                roles=[sup_role])
+        #sup_role.managers.append(superuser)
+
+        db.session.commit()
+
+        return
+
+create_roles_and_superuser()
+
